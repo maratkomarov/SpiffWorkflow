5a0d78f8cfe2106e9cad09aba8a5d6e0e02a5d1e
Marat Komarov <marat@scalr.com>
2013-05-04 10:23:54 +0300

    Force absolume imports to import celery from system modules

--- SpiffWorkflow/specs/Celery.py
+++ SpiffWorkflow/specs/Celery.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+from __future__ import absolute_import
+
 import logging
 
 from SpiffWorkflow.Task import Task
@@ -28,6 +30,7 @@ except ImportError:
     have_celery = False
 else:
     have_celery = True
+
 
 LOG = logging.getLogger(__name__)
 
